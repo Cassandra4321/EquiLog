464043a4d5f0b618e396d1ea8a81d0bfb301d8f1
Cassandra <cassandra.lidman@outlook.com>
2025-07-13 17:01:57 +0200

    First commit: Created sln and projects

---
+++ Backend/EquiLog/EquiLog.Services/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EquiLog.Services
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
