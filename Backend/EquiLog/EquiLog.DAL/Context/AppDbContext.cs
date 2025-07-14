b4e6e2aeba13e352411e98f848b0d676fa3ccc93
Cassandra <cassandra.lidman@outlook.com>
2025-07-14 11:22:22 +0200

    RoleSeeder, UserSeeder for superadmin(stable owner) and usersecrets in csproj, and first migration

---
+++ Backend/EquiLog/EquiLog.DAL/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EquiLog.DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquiLog.DAL.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
